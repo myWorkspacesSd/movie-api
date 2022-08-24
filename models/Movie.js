599ecdac3cbe3f430b67eb609012d1d4aa0761cf
myWorkspacesSd <myWorkspacesSd>
2022-08-24 14:48:15 +0300

    movie validations

--- models/Movie.js
+++ models/Movie.js
@@ -2,29 +2,37 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const MovieSchema = new schema({
-    directorId:{
-      type: schema.Types.ObjectId,
-    },
-    title:{
-        type: String,
-        required: true
-    },
-    category: {
-    	type: String
-		},
-    country: {
-			type: String,
-		},
-    year: {
-    	type: Number,
-		},
-    imdb_score: {
-    	type: Number,
-		},
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  directorId: schema.Types.ObjectId,
+  title: {
+    type: String,
+    required:  [true, '`{PATH}` alanı zorunludur.'],
+    maxlength: [15, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır '],
+    minlength: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır.']
+  },
+  category: {
+    type: String,
+    maxlength: 30,
+    minlength: 1,
+  },
+  country: {
+    type: String,
+    maxlength: 30,
+    minlength: 1,
+  },
+  year: {
+    type: Number,
+    max: 2040,
+    min: 1900
+  },
+  imdb_score: {
+    type: Number,
+    max: 10,
+    min: 0
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now
+  }
 });
 
 module.exports = mongoose.model('movie', MovieSchema);
